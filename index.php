e202eab1cf20a8d6e761ffd4deb086068f829df7
MD. Al-Amin Hawladar <60581249+alamindevs@users.noreply.github.com>
2021-08-10 20:51:27 +0600

    phroute configer

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
-require_once "view/home.php";
+use Phroute\Phroute\Dispatcher;
+use Phroute\Phroute\Exception\HttpMethodNotAllowedException;
+use Phroute\Phroute\Exception\HttpRouteNotFoundException;
+use Phroute\Phroute\RouteCollector;
+
+require_once "vendor/autoload.php";
+// require_once "view/home.php";
+
+$router = new RouteCollector();
+
+require_once "routes.php";
+
+# NB. You can cache the return value from $router->getData() so you don't have to create the routes each request - massive speed gains
+$dispatcher = new Dispatcher( $router->getData() );
+try {
+    $response = $dispatcher->dispatch( $_SERVER['REQUEST_METHOD'], parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ) );
+} catch ( HttpMethodNotAllowedException $e ) {
+    echo $e->getMessage();
+    die();
+}
+
+// Print out the value returned from the dispatched function
+echo $response;
