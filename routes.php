8df5e70ce1d1edaf8d5c1da88187fa0b5443fd4b
MD. Al-Amin Hawladar <60581249+alamindevs@users.noreply.github.com>
2021-08-13 21:35:57 +0600

    product route create

--- routes.php
+++ routes.php
@@ -1,6 +1,8 @@
 <?php
 use App\Controllers\Frontend\HomeController;
+use App\Controllers\Frontend\ProductController;
 use App\Controllers\Frontend\UserController;
 
 $router->controller( '/', HomeController::class );
 $router->controller( '/user', UserController::class );
+$router->controller( '/product', ProductController::class );
