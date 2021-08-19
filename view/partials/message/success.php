f880f0d93c9ab20625ce10d49026332158d44ba4
MD. Al-Amin Hawladar <60581249+alamindevs@users.noreply.github.com>
2021-08-19 18:04:35 +0600

    success notification create with toaster js

---
+++ view/partials/message/success.php
@@ -0,0 +1,21 @@
+<?php
+    if ( !empty( $_SESSION['message_success'] ) ) {
+        $message = $_SESSION['message_success'];
+    ?>
+
+<script>
+    $.toast({
+            heading: 'Success',
+            text: "<?=$message?>",
+            position: 'top-left',
+            loaderBg:'#ff6849',
+            icon: 'success',
+            hideAfter: 3000,
+            stack: 6
+        });
+</script>
+
+<?php }
+
+unset( $_SESSION['message_success'] )
+?>
