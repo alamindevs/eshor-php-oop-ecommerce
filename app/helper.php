1c239e7bb9d8d0e60eeaab5d31bc06c837c73411
MD. Al-Amin Hawladar <60581249+alamindevs@users.noreply.github.com>
2021-08-19 16:02:17 +0600

    redirect method create

--- app/helper.php
+++ app/helper.php
@@ -1,11 +1,22 @@
 <?php
 namespace App\Helper;
+
 function view( $view ) {
 
     if ( !empty( $view ) ) {
         require_once __DIR__ . '../../view/' . $view . '.php';
     } else {
         echo 'no view ';
+    }
+
+}
+
+if ( !function_exists( 'redirect' ) ) {
+
+    function redirect( $value = null ) {
+
+        return header( "Location: /$value" );
+
     }
 
 }
