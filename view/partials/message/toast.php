ebfc841eb8e836b2c1b2ebed81fb8a68b1ca04ae
MD. Al-Amin Hawladar <60581249+alamindevs@users.noreply.github.com>
2021-08-19 18:10:23 +0600

    add error notification

--- view/partials/message/toast.php
+++ view/partials/message/toast.php
@@ -17,5 +17,27 @@
 
 <?php }
 
-unset( $_SESSION['message_success'] )
+    unset( $_SESSION['message_success'] )
+?>
+
+<?php
+    if ( !empty( $_SESSION['message_error'] ) ) {
+        $message = $_SESSION['message_error'];
+    ?>
+
+<script>
+    $.toast({
+            heading: 'Error',
+            text: "<?=$message?>",
+            position: 'top-left',
+            loaderBg:'#ff6849',
+            icon: 'success',
+            hideAfter: 3000,
+            stack: 6
+        });
+</script>
+
+<?php }
+
+unset( $_SESSION['message_error'] )
 ?>
