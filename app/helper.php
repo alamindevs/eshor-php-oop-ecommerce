899b750ceefe1f5f492ad4a57ad2639b330dec99
MD. Al-Amin Hawladar <60581249+alamindevs@users.noreply.github.com>
2021-09-07 15:00:07 +0600

    partials_view helper function create

--- app/helper.php
+++ app/helper.php
@@ -1,13 +1,14 @@
 <?php
+if ( !function_exists( 'view' ) ) {
+    function view( $view ) {
 
-function view( $view ) {
+        if ( !empty( $view ) ) {
+            require_once __DIR__ . '../../view/' . $view . '.php';
+        } else {
+            echo 'no view ';
+        }
 
-    if ( !empty( $view ) ) {
-        require_once __DIR__ . '../../view/' . $view . '.php';
-    } else {
-        echo 'no view ';
     }
-
 }
 
 if ( !function_exists( 'redirect' ) ) {
@@ -28,4 +29,16 @@ if ( !function_exists( 'bcrypt' ) ) {
 
     }
 
+}
+
+if ( !function_exists( 'partials_view' ) ) {
+    function partials_view( $view ) {
+
+        if ( !empty( $view ) ) {
+            require_once __DIR__ . '../../view/' . $view . '.php';
+        } else {
+            echo 'no view ';
+        }
+
+    }
 }
