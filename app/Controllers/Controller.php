b6d5fd70a490c0634ec7b0950d10a38e0b7bd22b
MD. Al-Amin Hawladar <60581249+alamindevs@users.noreply.github.com>
2021-09-07 14:51:51 +0600

    remove view method

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -3,13 +3,4 @@ namespace App\Controllers;
 
 class Controller {
 
-    public function view( $view ) {
-        if ( !empty( $view ) ) {
-            require_once __DIR__ . '/../../view/' . $view . '.php';
-        } else {
-            echo 'no view ';
-        }
-
-    }
-
 }
